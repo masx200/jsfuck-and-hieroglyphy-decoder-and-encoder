fac08c6e6499a55d078f07546c8cb2797c251690
masx200 <34191203+masx200@users.noreply.github.com>
2021-09-18 14:50:46 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,20 @@ import { defineConfig } from "vite";
 import babel from "@rollup/plugin-babel";
 import { resolve } from "path";
 const root = resolve(__dirname, "src");
+import { minifyHtml } from "vite-plugin-html";
 export default defineConfig({
     root,
     plugins: [
+        minifyHtml({ removeAttributeQuotes: false }),
         babel({
             babelHelpers: "bundled",
             configFile: resolve(__dirname, "babel.config.js"),
         }),
     ],
     build: {
+        terserOptions: {
+            compress: { drop_console: true, drop_debugger: true },
+        },
         target: "es2015",
         rollupOptions: {
             input: [
