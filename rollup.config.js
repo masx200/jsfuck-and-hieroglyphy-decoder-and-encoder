243ef57216a899d815c589bbe3cfcc05ea6cded0
masx200 <masx200@qq.com>
2021-09-21 13:28:05 +0800

    优化es模块

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "rollup";
+import { resolve } from "path";
 import commonjs from "@rollup/plugin-commonjs";
 import noderesolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import ts from "rollup-plugin-ts";
+import babel from "@rollup/plugin-babel";
 export default defineConfig({
     plugins: [
         noderesolve(),
         commonjs(),
         ts({}),
+        babel({
+            babelHelpers: "bundled",
+            configFile: resolve(__dirname, "babel.config.js"),
+        }),
         terser({
             compress: { drop_console: true, drop_debugger: true },
             output: { beautify: true, comments: false },
