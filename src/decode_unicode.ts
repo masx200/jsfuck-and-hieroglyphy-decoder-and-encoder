55326742d15355dfb56b66c3cac746404d821541
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-03 15:34:02 +0800

    测试

--- src/decode_unicode.ts
+++ src/decode_unicode.ts
@@ -1,5 +1,5 @@
 "use strict";
 function decode_unicode(code: string) {
-    return unescape(code.replaceAll("\\", "%"));
+    return unescape(code.replaceAll("\\u", "%u"));
 }
 export { decode_unicode };
